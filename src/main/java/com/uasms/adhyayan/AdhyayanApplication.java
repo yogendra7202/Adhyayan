e4f89dc910dfd8c08149ca513a1b223a0cd4caf3
yogendrs <yogendra.singh.ext@nokia.com>
2024-09-20 15:54:19 +0530

    Init commit

---
+++ src/main/java/com/uasms/adhyayan/AdhyayanApplication.java
@@ -0,0 +1,13 @@
+package com.uasms.adhyayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhyayanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhyayanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uasms/adhyayan/AdhyayanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasms.adhyayan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhyayanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
